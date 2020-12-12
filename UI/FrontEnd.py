9c262d18e3dfa1d28febbed4c8a774637c59e721
nxb9626 <nxb9626@rit.edu>
2020-12-12 00:21:41 -0500

    Adjusted code based on comments on PR#14

--- UI/FrontEnd.py
+++ UI/FrontEnd.py
@@ -5,12 +5,13 @@ from SystemConnectors import WindowsConnection
 from functools import partial
 
 
-class FrontEnd():
+class FrontEnd:
     """Handles the Settings UI"""
     
     def __init__(self, volume_mixer):
         """
         Initializes FrontEnd
+
         :param volume_mixer: Volume Mixer used to communicate with rest of application
         """
         self.volume_mixer = volume_mixer
@@ -20,26 +21,28 @@ class FrontEnd():
     def open_color_dialog(self, knob):
         """
         Opens color picker and passes selected color to Volume Mixer
+
         :param knob: Knob number
         """
         color = askcolor()[0]
         color = [int(x) for x in color]
-        print(knob, color)
         self.volume_mixer.modify_application(knob, ChangedValue.COLOR, color)
 
    
     def get_apps(self):
         """
         Gets apps for dropdown, occurs every time dropdown is opened
+
         :return: list of app names with last 4 characters removed
         """
         self.open_apps = self.volume_mixer.get_running_applications()
-        return [app.name[0: -4] for app in self.open_apps]
+        return [app.name for app in self.open_apps]
 
     
     def update_app(self, knob, app_name):
         """
         Sends app seleced from dropdown to volumemixer, tied with knob
+
         :param knob Knob number 
         :param app_name: name of selected app
         """
@@ -48,7 +51,6 @@ class FrontEnd():
                 app = i
                 break
         self.volume_mixer.set_application(knob, app)
-        print(knob, app)
   
     
     def launch(self):
@@ -62,7 +64,8 @@ class FrontEnd():
         
         knobs = self.volume_mixer.applications
         for knob in range(len(knobs)):
-            Label(main_frame, text=knob).grid(row=1, column=knob, padx=5, pady=5)
+            Label(main_frame, text=knob)\
+                .grid(row=1, column=knob, padx=5, pady=5)
 
             Button(main_frame, text='Color', bg='Grey', \
                 command=partial(self.open_color_dialog, knob)) \
