97062ba00cbfeb2e1f37be7e69b942ab2c8cbb86
Michael-Longley <84longleym@gmail.com>
2020-12-12 21:21:50 -0500

    more clean version of this without 2n

--- SystemConnectors/WindowsConnector/ProcessController.py
+++ SystemConnectors/WindowsConnector/ProcessController.py
@@ -15,7 +15,6 @@ class ProcessController:
         for session in sessions:
             if session.Process and session.Process.name():
                 name = session.Process.name()
-                if session.Process.name.contains("."):
-                    name = name[0:name.rindex(".")]
-                process_names.append(name)
+                cleanName = name[0:name.rfind(".")]
+                process_names.append(cleanName != -1 if cleanName else name)
         return process_names
