220da2560a0526dcb74434e2b66939ce107b808b
nxb9626 <nxb9626@rit.edu>
2020-12-13 21:44:06 -0500

    Removed ChangedValue and if __name__ == "__main__"

--- Main.py
+++ Main.py
@@ -1,11 +1,10 @@
-from VolumeLogic import ChangedValue, VolumeMixer
+from VolumeLogic import VolumeMixer
 from SystemConnectors import WindowsConnection
 from UI import FrontEnd
-
-if __name__ == '__main__':
-    """
-    Starts up the backend and launches the front end
-    """
-    os_connection = WindowsConnection()
-    volume_mixer = VolumeMixer(os_connection)
-    FrontEnd(volume_mixer).launch()
+    
+"""
+Starts up the backend and launches the front end
+"""
+os_connection = WindowsConnection()
+volume_mixer = VolumeMixer(os_connection)
+FrontEnd(volume_mixer).launch()
