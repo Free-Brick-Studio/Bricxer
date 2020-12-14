30f22fe1b7e25d334f975a928d4480e191dc04ec
nxb9626 <nxb9626@rit.edu>
2020-12-13 21:45:49 -0500

    Added back __name__=="__main__"

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 from VolumeLogic import VolumeMixer
 from SystemConnectors import WindowsConnection
 from UI import FrontEnd
-    
-"""
-Starts up the backend and launches the front end
-"""
-os_connection = WindowsConnection()
-volume_mixer = VolumeMixer(os_connection)
-FrontEnd(volume_mixer).launch()
+
+if __name__ == '__main__':
+    """
+    Starts up the backend and launches the front end
+    """
+    os_connection = WindowsConnection()
+    volume_mixer = VolumeMixer(os_connection)
+    FrontEnd(volume_mixer).launch()
